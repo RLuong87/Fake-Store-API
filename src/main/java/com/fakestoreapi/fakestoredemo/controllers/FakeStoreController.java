0abce5d3c80e0df7adfc23048bf296339b7d2d42
Richies Luong <richiesluong.student@careerdevs.com>
2021-12-08 18:54:51 -0500

    Added some new methods

--- src/main/java/com/fakestoreapi/fakestoredemo/controllers/FakeStoreController.java
+++ src/main/java/com/fakestoreapi/fakestoredemo/controllers/FakeStoreController.java
@@ -13,12 +13,14 @@ public class FakeStoreController {
 
     public static final String FSURL = "https://fakestoreapi.com/products";
 
+    // Works
     @GetMapping("/getproducts")
     public Object getProducts(RestTemplate restTemplate) {
 
         return restTemplate.getForObject(FSURL, FakeStoreModel[].class);
     }
 
+    // Works
     @GetMapping("/{id}")
     public Object getOneProduct(RestTemplate restTemplate,
                                 @PathVariable String id) {
@@ -39,6 +41,7 @@ public class FakeStoreController {
         }
     }
 
+    // Not working
     @GetMapping("/someprices")
     public Object getPrice(RestTemplate restTemplate,
                            @RequestParam(name = "price") String price) {
@@ -55,6 +58,73 @@ public class FakeStoreController {
             }
         }
         return prices;
+    }
+
+    // Works
+    @GetMapping("/products")
+    public ArrayList<FakeStoreModel> getLimit(RestTemplate restTemplate, @RequestParam(name = "limit") String limit) {
+
+        int n = Integer.parseInt(limit);
+
+        ArrayList<FakeStoreModel> products = new ArrayList<>();
+
+        for (int i = 1; i <= n; i++) {
+
+            String URL = FSURL + "/" + i;
+            FakeStoreModel fakeProducts = restTemplate.getForObject(URL, FakeStoreModel.class);
+            products.add(fakeProducts);
+        }
+        return products;
+    }
+
+    // // Not working
+    @GetMapping("/{rate}")
+    public ArrayList<FakeStoreModel.Rating> getRatings(RestTemplate restTemplate, @PathVariable String rate) {
+
+        int rating = Integer.parseInt(rate);
+        ArrayList<FakeStoreModel.Rating> fourAndUp = new ArrayList<>();
+
+        FakeStoreModel.Rating fs = new FakeStoreModel.Rating();
+
+        int fourRating = Integer.parseInt(fs.getRate());
+
+
+        if (rating >= fourRating) {
+            FakeStoreModel.Rating fakeProducts = restTemplate.getForObject(FSURL, FakeStoreModel.Rating.class);
+            fourAndUp.add(fakeProducts);
+        }
+
+        for (FakeStoreModel.Rating fsm : fourAndUp) {
+
+        }
+        return fourAndUp;
+    }
+
+    // Works
+    @GetMapping("/jewelery")
+    public Object getJewelery(RestTemplate restTemplate) {
+
+        String URL = FSURL + "/category/jewelery";
+
+        return restTemplate.getForObject(URL, FakeStoreModel[].class);
+    }
+
+    // Not working
+    @GetMapping("/mensclothing")
+    public Object getMensClothing(RestTemplate restTemplate) {
+
+        String URL = FSURL + "/category/mensclothing";
+
+        return restTemplate.getForObject(URL, FakeStoreModel[].class);
+    }
+
+    // Works
+    @GetMapping("/electronics")
+    public Object getElectronics(RestTemplate restTemplate) {
+
+        String URL = FSURL + "/category/electronics";
+
+        return restTemplate.getForObject(URL, FakeStoreModel[].class);
     }
 
 }
